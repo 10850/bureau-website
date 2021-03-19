e4259e271a952d803e4517b7d02ba5e02992fdc0
10850 <10850@student.eadania.dk>
2021-03-19 16:57:01 +0100

    bureau site template med menu

---
+++ nav.js
@@ -0,0 +1,52 @@
+if(screen.width < 768 + "px") { 
+    //load file for screen width 1000 and up here
+    const hamburger = document.getElementById("menu-btn");
+    const menuContainer = document.getElementById("menu-lists-container");
+    let menuOpen = false;
+
+    console.log(hamburger)
+    // åben burger menu on click
+    hamburger.addEventListener('click', () => {
+        if (menuOpen == false){
+            menuContainer.style.left = 0;
+            menuOpen = true;
+        } else {
+            menuContainer.style.left = -100 + "%";
+            menuOpen = false;
+        }
+    });
+
+    // åben dropdown on click
+    const servOpener = document.getElementById("serv-opener");
+    const casesOpener = document.getElementById("cases-opener");
+    const dropdownServ = document.getElementById('dropdown-serv');
+    const dropdownCases = document.getElementById('dropdown-cases');
+    let dropdownOpen1 = false;
+    let dropdownOpen2 = false;
+
+    casesOpener.addEventListener('click', () => {
+            if (dropdownOpen1 == false){
+                dropdownCases.style.display = "list-item";
+                dropdownOpen1 = true;
+            } else {
+                dropdownCases.style.display = "none";
+                dropdownOpen1 = false;
+            }
+        })
+
+    servOpener.addEventListener('click', () => {
+            if (dropdownOpen2 == false){
+                dropdownServ.style.display = "list-item";
+                dropdownOpen2 = true;
+            } else {
+                dropdownServ.style.display = "none";
+                dropdownOpen2 = false;
+            }
+        })
+
+} else {
+    
+}
+
+
+    
