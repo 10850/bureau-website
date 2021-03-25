09f24d6a71937d1f814414bdb999dfabd28350ba
10850 <10850@student.eadania.dk>
2021-03-25 09:29:39 +0100

    fixet burgermenu

--- nav.js
+++ nav.js
@@ -3,22 +3,37 @@ const menuContainer = document.getElementById("menu-lists-container");
 let menuOpen = false;
 
 if(screen.width < 768 ) {
-    console.log(hamburger)
     // åben burger menu on click
     hamburger.addEventListener('click', () => {
+        console.log(menuOpen);
         if (menuOpen == false){
             menuContainer.style.left = 0;
             menuOpen = true;
+            console.log(menuOpen);
         } else {
             menuContainer.style.left = -100 + "%";
             menuOpen = false;
+            console.log(menuOpen);
         }
     });
 
-    // åben dropdown on click
+    
+
+    // åben dropdown on clicks
     const servOpener = document.getElementById("serv-opener");
     const dropdownServ = document.getElementById('dropdown-serv');
     let dropdownOpen2 = false;
+
+    servOpener.addEventListener('click', () => {
+        if (dropdownOpen2 == false){
+            dropdownServ.style.display = "list-item";
+            dropdownOpen2 = true;
+        } else {
+            dropdownServ.style.display = "none";
+            dropdownOpen2 = false;
+        }
+    })
+}
     
     //const casesOpener = document.getElementById("cases-opener");
     //const dropdownCases = document.getElementById('dropdown-cases');
@@ -34,19 +49,3 @@ if(screen.width < 768 ) {
     //          dropdownOpen1 = false;
     //      }
     // })
-
-    servOpener.addEventListener('click', () => {
-            if (dropdownOpen2 == false){
-                dropdownServ.style.display = "list-item";
-                dropdownOpen2 = true;
-            } else {
-                dropdownServ.style.display = "none";
-                dropdownOpen2 = false;
-            }
-        })
-
-    }else {
-            menuContainer.style.left = 0;
-          }
-
-    
